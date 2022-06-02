0688c7dcbf09feb24117c1ba93bcba119a02eec0
fabian-netto <fabiannetto666@gmail.com>
2022-06-02 11:31:59 +0530

    New pages

--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
 from cgi import print_form
 import email
+from logging import exception
 from re import A, template
+import re
 from unicodedata import name
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from store.device_functions import check_finger_return_id
 
 from store.forms import userform
 from .models import *
@@ -12,149 +15,173 @@ from .forms import *
 import serial
 import time
 
+
 def store(request):
-	products = Product.objects.all()
-	context = {'products':products}
-	return render(request, 'store.html', context)
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store.html', context)
+
 
 def product(request, pk):
-	product = Product.objects.get(id=pk)
 
-	if request.method == 'POST':
-		product = Product.objects.get(id=pk)
-		#Get user account information
-		try:
-			customer = request.user.customer	
-		except:
-			device = request.COOKIES['device']
-			customer, created = Customer.objects.get_or_create(device=device)
+    product = Product.objects.get(id=pk)
 
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
-		orderItem.quantity=request.POST['quantity']
-		orderItem.save()
+    if request.method == 'POST':
+        product = Product.objects.get(id=pk)
+        # Get user account information
+        try:
+            customer = request.user.customer
+        except:
+            device = request.COOKIES['device']
+            customer, created = Customer.objects.get_or_create(device=device)
 
-		return redirect('cart')
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        orderItem, created = OrderItem.objects.get_or_create(
+            order=order, product=product)
+        orderItem.quantity = request.POST['quantity']
+        orderItem.save()
 
-	context = {'product':product}
-	return render(request, 'product.html', context)
+        return redirect('cart')
+
+    context = {'product': product}
+    return render(request, 'product.html', context)
+
 
 def cart(request):
-	try:
-		customer = request.user.customer
-	except:
-		device = request.COOKIES['device']
-		customer, created = Customer.objects.get_or_create(device=device)
+    try:
+        customer = request.user.customer
+    except:
+        device = request.COOKIES['device']
+        customer, created = Customer.objects.get_or_create(device=device)
 
-	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
 
-	context = {'order':order}
-	return render(request, 'cart.html', context)
+    context = {'order': order}
+    return render(request, 'cart.html', context)
+
 
 def recharge(request):
-	if request.POST:
-		fingid = 1
-		numb = request.POST['numb']
-		print(numb)
-		
+    if request.POST:
+        fingid = 1
+        numb = request.POST['numb']
+        print(numb)
 
-		c = Customer.objects.get(id=fingid)
-		
-		c.amount = c.amount + int(numb)
-		c.save()
+        c = Customer.objects.get(id=fingid)
 
-		return redirect(auth_recharge)
-		
-	return render(request, 'recharge.html')
+        c.amount = c.amount + int(numb)
+        c.save()
+
+        auth_recharge(request, numb)
+
+    return render(request, 'recharge.html')
+
 
 def delete_event(request, pk):
-	ord = OrderItem.objects.get(id=pk)
-	ord.delete()
-	return redirect('cart')
+    ord = OrderItem.objects.get(id=pk)
+    ord.delete()
+    return redirect('cart')
+
 
 def checkout(request):
-	return render(request, "checkout.html")
-		
+    return render(request, "checkout.html")
+
+
 def detail(request):
-	print("Check fingerprint")
-	import serial.tools.list_ports
+    fingid = check_finger_return_id('c')
 
-	currentPort = None
+    # TODO:All the error cases from the function and the associated error pages
+    switcher = {
+        -1: redirect(user_not),
+        -2: redirect(device_not),
+        -3: redirect(device_busy),
+        -4: redirect(timeout)
+    }
 
-	ports = list(serial.tools.list_ports.comports())
-	# print("port port is", ports[0])
-	for p in ports:
-		print(p.description)
-		if "CP210x" in p.description:
-			currentPort = p
-			break
+    if(int(fingid) < 0):
+        return switcher.get(fingid, HttpResponse("Error"))
 
-	if currentPort == None:
-		print("No device found")
-		return
-	print("port is", currentPort.device)
+    try:
+        customer = request.user.customer
+    except:
+        device = request.COOKIES['device']
+        customer, created = Customer.objects.get_or_create(device=device)
 
-	arduino = serial.Serial(port=currentPort.device,
-							baudrate=9600, timeout=.1)
+    order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
 
-	arduino.write(bytes('c', 'utf-8'))
-	arduino.reset_input_buffer()
+    cust = Customer.objects.get(id=fingid)
 
-	while(not arduino.in_waiting):
-		print("waiting...")
-		time.sleep(0.5)
+    if(cust.amount - order.get_cart_total < 0):
+        # TODO: This condition
+        return redirect(insuff_bal)
 
-	fingid = arduino.readline().decode('utf-8')
-	print('The detected ID is ', id)
-	arduino.write(bytes('x', 'utf-8'))
+    cust.amount = cust.amount - order.get_cart_total
+    cust.save()
 
-	try:
-		customer = request.user.customer
-	except:
-		device = request.COOKIES['device']
-		customer, created = Customer.objects.get_or_create(device=device)
+    cart_total = order.get_cart_total
+    order.delete()
 
-	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    context = {'cust': cust, 'cart_total': cart_total}
 
-	
-	cust = Customer.objects.get(id=fingid)
-	context = {'cust':cust,'order':order}
-	return render(request, "detail.html",context)
-	# return HttpResponse('Figerprint Match Found. ID: '+id)
-	
+    return render(request, "detail.html", context)
+    # return HttpResponse('Figerprint Match Found. ID: '+id)
+
+
 def detail_recharge(request):
-	return render(request, 'detail_recharge.html')
+    return render(request, 'detail_recharge.html')
 
-def auth_recharge(request):
-	
-	return render(request, 'auth_recharge.html')		
+
+def auth_recharge(request, numb):
+	print(numb)
+	return render(request, 'auth_recharge.html')
+
 
 def auth_register(request):
-	return render(request, 'auth_register.html')		
+    return render(request, 'auth_register.html')
+
 
 def reciept(request):
-	try:
-		customer = request.user.customer
-	except:
-		device = request.COOKIES['device']
-		customer, created = Customer.objects.get_or_create(device=device)
+    try:
+        customer = request.user.customer
+    except:
+        device = request.COOKIES['device']
+        customer, created = Customer.objects.get_or_create(device=device)
 
-	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
 
-	context = {'order':order}
-	return render(request, 'reciept.html', context)
+    context = {'order': order}
+    return render(request, 'reciept.html', context)
+
 
 def register(request):
-	if request.POST:
-		fingid= 1
-		name = request.POST['name']
-		email = request.POST['email']
-		print(name,email)
+    if request.POST:
+        fingid = 1
+        name = request.POST['name']
+        email = request.POST['email']
+        print(name, email)
 
-		c = Customer.objects.get(id=fingid)   	
-		c.name = name
-		c.email = email
-		c.save()
+        c = Customer.objects.get(id=fingid)
+        c.name = name
+        c.email = email
+        c.save()
 
-		return redirect(auth_register)
-	return render(request, 'register.html')
+        return redirect(auth_register)
+    return render(request, 'register.html')
+
+def user_not(request):
+	return render(request, 'user_not.html')	
+
+def device_not(request):
+	return render(request, 'device_not.html')	
+
+def device_busy(request):
+	return render(request, 'device_busy.html')	
+
+def timeout(request):
+	return render(request, 'timeout.html')	
+
+def insuff_bal(request):
+	return render(request, 'insuff_bal.html')	
